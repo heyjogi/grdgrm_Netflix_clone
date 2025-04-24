e0ac717c5fae97dfc3b365ded9722be91df730a7
Changyeol Choe <duf325@naver.com>
2025-04-24 23:16:04 +0900

    fix: 헤더 프로필 이미지 cdn 링크 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -82,7 +82,7 @@ export default function Header() {
           <div className="account-tab">
             <img
               className="profile-icon"
-              src="https://occ-0-8133-58.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+              src="https://occ-0-8232-988.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
               alt=""
             />
           </div>
