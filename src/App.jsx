a64e18818d954429679bc43cb0c8c18b5211ecd5
Changyeol Choe <duf325@naver.com>
2025-04-26 00:33:27 +0900

    feat: 검색 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Search from "./pages/SearchPage/index.jsx";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/search" element={<Search />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
