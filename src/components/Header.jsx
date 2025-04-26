78f5ba4201ccdf05be4929efc53da9a537f8c1d2
Changyeol Choe <duf325@naver.com>
2025-04-26 13:09:57 +0900

    perf: 프로필 이미지 호버시 옆에 로테이트되는 역삼각형 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -150,11 +150,14 @@ export default function Header() {
                     </button>
                 </div>
                 <div className="nav-element profile-wrapper">
-                    <img
-                        className="profile-icon"
-                        src="https://occ-0-8232-988.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
-                        alt="Netflix Profile Icon"
-                    />
+                    <div className="profile-trigger">
+                        <img
+                            className="profile-icon"
+                            src="https://occ-0-8232-988.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+                            alt="Netflix Profile Icon"
+                        />
+                        <span className="triangle-icon"></span>
+                    </div>
                     <div className="profile-dropdown">
                         <ul>
                             <li>
