b59acc16ff5a8be12ca87afbc3e72207c8afbd42
한지우 <hjw0235@naver.com>
2025-04-28 01:28:48 +0900

    일부 컴포넌트 반응형 구현 및 드롭메뉴 구현

--- src/components/TrailerSection.jsx
+++ src/components/TrailerSection.jsx
@@ -94,8 +94,44 @@ export default function TrailerSection() {
                         미스터리를 해결한다.
                     </div>
                     <div className="trailer-actions-btns">
-                        <button className="play-btn">▶ 재생</button>
-                        <button className="info-btn">ℹ️ 상세 정보</button>
+                        <button className="play-btn">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            role="img"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                            data-icon="PlayStandard"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+                            ></path>
+                          </svg>
+                          <span>재생</span>
+                        </button>
+                        <button className="info-btn">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            role="img"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                            data-icon="CircleIStandard"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z"
+                              clipRule="evenodd"
+                              fillRule="evenodd"
+                            ></path>
+                          </svg>
+                           <span>상세 정보</span>
+                        </button>
                     </div>
                 </div>
 
@@ -171,7 +207,20 @@ export default function TrailerSection() {
                             )}
                         </button>
                     )}
-                    <div className="age-badge">15+</div>
+                    <div className="age-badge">
+                      <svg id="maturity-rating-977" 
+                        viewBox="0 0 100 100" 
+                        className="age-img">
+                        <path id="Fill---Orange" 
+                          fill="#CD6D34" 
+                          d="M88.727 100H11.27C5.05 100 0 94.952 0 88.727V11.273C0 5.047 5.05 0 11.27 0h77.457C94.952 0 100 5.047 100 11.273v77.454C100 94.952 94.952 100 88.727 100">
+                          </path>
+                          <path id="path15" 
+                            fill="#FFFFFE" 
+                            d="M36.876 15.482v68.651H21.509v-49.51h-5.484l7.097-19.141h13.754zm45.46 0V28.87H57.175v10.063h24.08c.845 0 1.533.687 1.533 1.534v42.13c0 .845-.688 1.532-1.534 1.532H43.616a1.533 1.533 0 01-1.533-1.533V62.202H57v8.988h10.874V52.052h-25.79v-36.57h40.254z">
+                          </path>
+                      </svg>
+                    </div>
                 </div>
             </div>
         </div>
